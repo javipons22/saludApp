f2f0ac807e29b96a0fd35bac499521f3729dd38e
javier <javipons@mail.com>
2019-06-10 12:33:49 -0300

    problema arreglado en functions php

--- functions.php
+++ functions.php
@@ -4,14 +4,16 @@ function wpt_theme_styles()
 { //wpt es un nombre dado por nosotros para diferenciar con otros plugins
     wp_enqueue_style('date_picker_css', get_template_directory_uri() . '/css/jquery.datetimepicker.min.css');
     wp_enqueue_style('reset_css', get_template_directory_uri() . '/css/reset.css');
+    wp_enqueue_style('hamb_icon_css', get_template_directory_uri() . '/css/hamb-icon.css');
     wp_enqueue_style('fuentegoogle', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&display=swap');
     wp_enqueue_style('main_css', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('laptop_css', get_template_directory_uri() . '/css/laptop.css');
 }
 add_action('wp_enqueue_scripts', 'wpt_theme_styles');
 
 function wpt_theme_js()
 {
-    wp_enqueue_script('fontawesome_js', get_template_directory_uri() . '/js/fontawesome.js', '', '', false); //el ultimo parametro es dependence , version y si debe aparecer en el footer(false)
+    wp_enqueue_script('fontawesome_js', get_template_directory_uri() . '/js/all.js', '', '', false); //el ultimo parametro es dependence , version y si debe aparecer en el footer(false)
     wp_enqueue_script('date_picker_js', get_template_directory_uri() . '/js/jquery.datetimepicker.full.min.js', array('jquery'), '', true);
     wp_enqueue_script('app_js', get_template_directory_uri() . '/js/app.js', array('jquery'), '', true);
 }
@@ -42,6 +44,24 @@ function custom_login_fail($username)
     }
 }
 
+// Si alguno o todos los campos estan vacios redireccionar a pantalla de login en vez de abrir wp-login.php
+add_filter('authenticate','custom_authenticate', 31, 3);
+function custom_authenticate( $user, $username, $password ) {
+ 
+    if ( is_wp_error( $user ) && isset( $_SERVER[ 'HTTP_REFERER' ] ) && !strpos( $_SERVER[ 'HTTP_REFERER' ], 'wp-admin' ) && !strpos( $_SERVER[ 'HTTP_REFERER' ], 'wp-login.php' ) ) {
+      $referrer = $_SERVER[ 'HTTP_REFERER' ];
+      foreach ( $user->errors as $key => $error ) {
+         if ( in_array( $key, array( 'empty_password', 'empty_username') ) ) {
+            unset( $user->errors[ $key ] );
+            $user->errors[ 'custom_'.$key ] = $error;
+         }
+      }
+    }
+ 
+    return $user;
+}
+
+
 add_action('wp_logout', 'auto_redirect_after_logout');
 function auto_redirect_after_logout()
 {
@@ -66,5 +86,3 @@ function blockusers_init()
         exit;
     }
 }
-
-
